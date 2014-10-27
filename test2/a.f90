12d76ec1117b79f74b3426464c65b10076ea9951
zxt <zxtdocument@126.com>
2014-10-26 20:38:14 -0700

    00

---
+++ test2/a.f90
@@ -0,0 +1,29 @@
+program a
+    implicit none
+    integer :: unit_out=10, unit_table=20
+    character(80)::hdstr='YEAR MNTH DAYS HOUR MINU SECO'
+    character(80)::obstr='BRIT'
+    real(8)::hdr(6), obs(1,15)
+    
+    integer::ireadmg, ireadsb
+    character(8) subset
+    integer::unit_in=10
+    integer::idate,iret,num_message,num_subset
+
+    open(unit_in,file='abufr',action='read',form='unformatted')
+    call openbf(unit_in,'IN',unit_in)
+    call datelen(10)
+
+        num_message=0
+        msg_report: do while (ireadmg(unit_in,subset,idate)==0)
+            num_message=num_message+1
+            num_subset=0
+            write(*,'(I10,I4,a10)') idate,num_message,subset
+            sb_report: do while (ireadsb(unit_in)==0)
+                num_subset=num_subset+1
+                call ufbint(unit_in,hdr,6,1,iret,hdstr)
+                write(*,'(2I8,6f8.1)') num_subset,iret,hdr
+            enddo sb_report
+        enddo msg_report
+        call closbf(unit_in)
+end
---
+++ test2/bufrtab.f90
@@ -0,0 +1,8 @@
+program bufrtab
+    integer :: unit_out=10,unit_table=20
+    open(unit_table,file='bufrtable.txt')
+    open(unit_out,file='bufr1old',status='old',form='unformatted')
+    call openbf(unit_out,'IN',unit_out)
+    call dxdump(unit_out,unit_table)
+    call closbf(unit_out)
+end
